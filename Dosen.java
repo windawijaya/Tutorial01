15e7b6990edefcb297ed5d7d20549718f1407e8d
Winda Wijaya <windanyaan64@gmail.com>
2018-09-07 14:37:58 +0700

    Initial Commit Dosen.java

---
+++ Dosen.java
@@ -0,0 +1,16 @@
+
+public class Dosen {
+	private String nDosen;
+	 
+	Mahasiswa(String nDosen){
+		this.nDosen = nDosen;	
+	}
+	
+	public String getName() {
+        return this.nDosen;
+    }
+ 
+    public void setName(String nDosen) {
+        this.nDosen = nDosen;
+    }
+}
