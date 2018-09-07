73e7de77d6ff0919613523e28fafb36e59edfc3c
Winda Wijaya <windanyaan64@gmail.com>
2018-09-07 14:31:39 +0700

    Initial Commit Mahasiswa.java

---
+++ Mahasiswa.java
@@ -0,0 +1,16 @@
+
+public class Mahasiswa {
+	private String nMahasiswa;
+	 
+	Mahasiswa(String nMahasiswa){
+		this.nMahasiswa = nMahasiswa;	
+	}
+	
+	public String getName() {
+        return this.nMahasiswa;
+    }
+ 
+    public void setName(String nMahasiswa) {
+        this.nMahasiswa = nMahasiswa;
+    }
+}
